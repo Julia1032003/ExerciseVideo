20614513c57bd91e55cb5a8b04eda70f5f4a82cd
Julia <julia1032003@gmail.com>
2019-08-13 11:49:47 +0800

    Initial Commit

---
+++ ExerciseVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExerciseVideo
+//
+//  Created by Julia Wang on 2019/8/13.
+//  Copyright © 2019 Julia Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
